fc2d6669e0779216af7be174a25609d7d7d8d1aa
JarvisCraft <mrjarviscraft@gmail.com>
2019-04-17 18:22:31 +0300

    Remove `Simple` from event names in `protocol`

--- protocol/src/main/java/org/feathercore/protocol/event/PreConnectionEvent.java
+++ protocol/src/main/java/org/feathercore/protocol/event/PreConnectionEvent.java
@@ -20,15 +20,13 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.feathercore.eventbus.SimpleCancellableEvent;
 import org.feathercore.protocol.Connection;
-import org.feathercore.protocol.packet.Packet;
 
 /**
  * Created by k.shandurenko on 16/04/2019
  */
 @Data
 @EqualsAndHashCode(callSuper = true)
-public class PacketReceiveEventSimple extends SimpleCancellableEvent {
+public class PreConnectionEvent extends SimpleCancellableEvent {
 
     private final Connection connection;
-    private final Packet packet;
 }
