7662fe0a65b8585b0446c7aede50da3008a9e898
PROgrammer_JARvis <mrjarviscraft@gmail.com>
2019-05-31 00:36:45 +0300

    Containment tweaks & fixes for ModuleLoader verified by unit-test

--- module-api/src/main/java/ru/feathercore/moduleapi/ModuleLoader.java
+++ module-api/src/main/java/ru/feathercore/moduleapi/ModuleLoader.java
@@ -17,8 +17,12 @@
 package ru.feathercore.moduleapi;
 
 import lombok.NonNull;
+import lombok.val;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * Loader of {@link Module modules} which uses {@link ModuleInitializer initializers} for this purpose.
@@ -26,6 +30,69 @@ import java.util.Collection;
  * @param <M> super-type of all modules managed
  */
 public interface ModuleLoader<M extends Module> {
+
+    /**
+     * Gets all modules loaded by this module loader.
+     *
+     * @return all loaded modules
+     *
+     * @apiNote modifications to returned collection should not have side-effect on this module loader
+     * however it is recommended to perform copying of it if loading/unloading modules while iterating
+     * so that {@link java.util.ConcurrentModificationException} does not happen
+     */
+    Collection<? extends M> getModules();
+
+    /**
+     * Gets all modules of given type loaded by this module loader.
+     *
+     * @param type type of modules for which to look
+     * @param <T> exact type of modules searched
+     * @return all loaded modules of given type
+     */
+    @SuppressWarnings("unchecked")
+    default <T extends M> Collection<? extends T> getModules(@NonNull final Class<T> type) {
+        return (Collection<? extends T>) getModules()
+                .stream()
+                .filter(module -> type.isAssignableFrom(module.getClass()))
+                .collect(Collectors.toSet());
+    }
+
+    /**
+     * Finds a module of given type loaded by this module loader.
+     *
+     * @param type type of module for which to look
+     * @param <T> exact type of module searched
+     * @return {@link Optional} containing a module of given type if it was found or an empty {@link Optional} otherwise
+     */
+    @SuppressWarnings("unchecked")
+    default <T extends M> Optional<? extends T> getModule(@NonNull final Class<T> type) {
+        return (Optional<? extends T>) getModules()
+                .stream()
+                .filter(module -> type.isAssignableFrom(module.getClass()))
+                .findAny();
+    }
+
+    /**
+     * Checks whether there is a module of given type currently loaded by this module loader.
+     *
+     * @param type type of module to get checked
+     * @return {@link true} if there is a module of given type currently loaded and {@link false} otherwise
+     *
+     * @apiNote multiple modules of given type may be loaded at once
+     */
+    default boolean isModuleLoaded(@NonNull final Class<? extends M> type) {
+        return getModules().stream().anyMatch(module -> type.isAssignableFrom(module.getClass()));
+    }
+
+    /**
+     * Checks whether the given module is currently loaded by this module loader.
+     *
+     * @param module module to check
+     * @return {@link true} if the module is loaded and {@link false} otherwise
+     */
+    default <T extends M> boolean isModuleLoaded(@NonNull final T module) {
+        return getModules().contains(module);
+    }
 
     /**
      * Loads a module provided by the specified initializer using the given configuration.
@@ -55,6 +122,20 @@ public interface ModuleLoader<M extends Module> {
     }
 
     /**
+     * Unloads all modules of the specified type.
+     *
+     * @param moduleTypes type of modules to unload
+     * @param <T> exact type of modules unloaded
+     * @return collection (possibly empty) of unloaded modules of given type
+     */
+    default <T extends M> Collection<? extends T> unloadModules(@NonNull final Class<T> moduleTypes) {
+        val modules = getModules(moduleTypes);
+        for (val loadedModule : modules) unloadModule(loadedModule);
+
+        return modules;
+    }
+
+    /**
      * Unloads the specified module.
      *
      * @param module module which should unloaded
@@ -64,22 +145,14 @@ public interface ModuleLoader<M extends Module> {
     <T extends M> boolean unloadModule(@NonNull T module);
 
     /**
-     * Gets all modules loaded by this module loader.
+     * Unloads all modules loaded at current time.
      *
-     * @return all loaded modules
-     *
-     * @apiNote modifications to returned collection should not have side-effect on this module loader
+     * @return all modules unloaded
      */
-    Collection<? extends M> getModules();
+    default Collection<? extends M> unloadAllModules() {
+        val modules = new ArrayList<>(getModules());
+        for (val loadedModule : modules) unloadModule(loadedModule);
 
-    /**
-     * Checks whether there is a module of given type currently loaded by this module loader.
-     *
-     * @param module class of module to be checked.
-     * @return {@link true} if the module is loaded and {@link false} otherwise
-     */
-    default boolean isModuleLoaded(Class<? extends M> module) {
-        return getModules().stream().anyMatch(mod -> module.isAssignableFrom(mod.getClass()));
+        return modules;
     }
-
 }
